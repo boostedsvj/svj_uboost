587349e79a3faf548662d5196dff7f0528714fe8
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2025-08-25 09:05:55 +0000

    Fixing analyis type flag

--- run_ddt_construct.sh
+++ run_ddt_construct.sh
@@ -7,9 +7,9 @@ python apply_DDT.py --analysis_type cut-based --rt_sel 1.18 --ddt_map_file ./mod
 
 # Running the original BDT-based selection (No RT DDT)
 rm ./models/bdt_ddt_map_ANv6.json
-python apply_DDT.py --analysis_type cut-based --rt_sel 1.18 --ddt_map_file ./models/bdt_ddt_map_ANv6.json --bkg_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Summer20UL*/QCD.npz" --plot 2D_DDT_map
-python apply_DDT.py --analysis_type cut-based --rt_sel 1.18 --ddt_map_file ./models/bdt_ddt_map_ANv6.json --bkg_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Summer20UL*/*.npz" --sig_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Private*/*.npz" --plot fom_significance
-python apply_DDT.py --analysis_type cut-based --rt_sel 1.18 --ddt_map_file ./models/bdt_ddt_map_ANv6.json --bkg_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Summer20UL*/*.npz" --sig_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Private*/*.npz" --var_cuts 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9  --plot bkg_scores_mt
+python apply_DDT.py --analysis_type BDT-based --rt_sel 1.18 --ddt_map_file ./models/bdt_ddt_map_ANv6.json --bkg_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Summer20UL*/QCD*.npz" --plot 2D_DDT_map
+python apply_DDT.py --analysis_type BDT-based --rt_sel 1.18 --ddt_map_file ./models/bdt_ddt_map_ANv6.json --bkg_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Summer20UL*/*.npz" --sig_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Private*/*.npz" --plot fom_significance
+python apply_DDT.py --analysis_type BDT-based --rt_sel 1.18 --ddt_map_file ./models/bdt_ddt_map_ANv6.json --bkg_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Summer20UL*/*.npz" --sig_files "root://cmseos.fnal.gov//store/user/lpcdarkqcd/boosted/skims_20241030_hadd/Private*/*.npz" --var_cuts 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9  --plot bkg_scores_mt
 
 # Runnin the RT ddt map
 rm ./models/rt_ddt_map_ANv6.json
