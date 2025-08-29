18195bdf427f0ffb0b5779ebc6492a4c2c3cd60a
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2025-08-29 06:25:52 +0000

    Changing default RT cut

--- run_ddt_construct.sh
+++ run_ddt_construct.sh
@@ -23,12 +23,12 @@ python apply_DDT.py --analysis_type RT --ddt_map_file ./models/rt_ddt_map_ANv6_3
 
 # Running the new ECF DDT with separate RT DDT
 # rm ./models/cutbased_ddt_map_withRT_ANv6_3d.json
-python apply_DDT.py --analysis_type cut-based --rt_sel 1.20 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file ./models/cutbased_ddt_map_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --sig_files "${SIG_ALL_FILES}"  --plot 2D_DDT_map fom_significance
-python apply_DDT.py --analysis_type cut-based --rt_sel 1.20 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file ./models/cutbased_ddt_map_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --sig_files "${SIG_ALL_FILES}"  --var_cuts 0.08 0.09 0.10 0.11 0.12 --plot bkg_scores_mt
+python apply_DDT.py --analysis_type cut-based --rt_sel 1.19 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file ./models/cutbased_ddt_map_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --sig_files "${SIG_ALL_FILES}"  --plot 2D_DDT_map fom_significance
+python apply_DDT.py --analysis_type cut-based --rt_sel 1.19 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file ./models/cutbased_ddt_map_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --sig_files "${SIG_ALL_FILES}"  --var_cuts 0.08 0.09 0.10 0.11 0.12 --plot bkg_scores_mt
 
 # Running the new BDT DTT with RT DDT
 # rm ./models/bdt_ddt_withRT_ANv6_3d.json
-python apply_DDT.py --analysis_type BDT-based --rt_sel 1.20 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file models/bdt_ddt_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --plot 2D_DDT_map
-python apply_DDT.py --analysis_type BDT-based --rt_sel 1.20 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file models/bdt_ddt_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --sig_files "${SIG_ALL_FILES}"  --plot fom_significance
-python apply_DDT.py --analysis_type BDT-based --rt_sel 1.20 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file models/bdt_ddt_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --var_cuts 0.4 0.5 0.6 0.7 0.8 --plot bkg_scores_mt
+python apply_DDT.py --analysis_type BDT-based --rt_sel 1.19 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file models/bdt_ddt_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --plot 2D_DDT_map
+python apply_DDT.py --analysis_type BDT-based --rt_sel 1.19 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file models/bdt_ddt_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --sig_files "${SIG_ALL_FILES}"  --plot fom_significance
+python apply_DDT.py --analysis_type BDT-based --rt_sel 1.19 --rt_ddt_file ./models/rt_ddt_map_ANv6_3d.json --ddt_map_file models/bdt_ddt_withRT_ANv6_3d.json --bkg_files "${BKG_ALL_FILES}" --var_cuts 0.4 0.5 0.6 0.7 0.8 --plot bkg_scores_mt
 
